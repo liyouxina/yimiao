d145c5d63f7bf39f06eb3af0929190e57f52d747
liyouxin.lyx <liyouxin.lyx@alibaba-inc.com>
2022-10-18 10:26:20 +0800

    more

--- web/app.js
+++ web/app.js
@@ -1,7 +1,45 @@
 var http = require('http')
 var rf = require("fs")
 var format = require('string-format')
-var RedisApi = require('./lib/redis')
+
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: '127.0.0.1', // 默认 host
+    port: '6379' // 默认端口
+});
+redisClient.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+const INLINE = "inline";
+const START = "start";
+const WAITING = "waiting";
+const WORKING = "working";
+const SUCCESS = "success";
+const WENAN = {
+    "inline": "正在排队",
+    "start": "开始抢",
+    "waiting": "等待有疫苗",
+    "working": "正在抢",
+    "success": "抢成功"
+}
+
+var RedisApi = {
+    "addJob": (idCard, city, name) => {
+        redisClient.set(`JOB_${idCard}_${city}_${name}`, INLINE);
+    },
+    "queryJob": async (idCard, city, name) => {
+        var result = null;
+        await redisClient.get(`JOB_${idCard}_${city}_${name}`, (err, value) => {
+            if (err) {
+                console.log(err);
+                return null;
+            }
+            result = value;
+        });
+        return result;
+    }
+}
 
 var getQueryParams = (url) => {
     if (url.split("?").length == 0) {
@@ -16,20 +54,32 @@ var getQueryParams = (url) => {
 }
 
 var handleApi = (req, res) => {
-    var url = req.url.trimStart("/api/");
+    var url = req.url.slice(5);
     if (url.startsWith("submitTask")) {
         var params = getQueryParams(req.url);
         RedisApi.addJob(params["idCard"], params["city"], params["name"]);
+        res.writeHead(200, {
+            'content-type': 'text/html;charset=utf8'
+        });
         res.write("成功提交任务，请耐心等待结果");
         res.end();
         return;
     }
-    if (url.startsWith("queryProcess")) {
+    if (url.startsWith("queryJob")) {
         var params = getQueryParams(req.url);
-        res.write(RedisApi.queryJob(params["idCard"], params["city"], params["name"]));
-        res.end();
+        aa = RedisApi.queryJob(params["idCard"], params["city"], params["name"]);
+        redisClient.get(`JOB_${params["idCard"]}_${params["city"]}_${params["name"]}`, (err, value) => {
+            if (err) {
+                console.log(err);
+                return null;
+            }
+            res.write(WENAN[value]);
+            res.end();
+        });
         return;
     }
+    res.write("404");
+    res.end();
 }
 
 var handle = (req, res) => {
